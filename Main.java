4f5358cc4d28f8820d3339eff4ecef47274aedc3
ConnorKohout <conmanmc330@gmail.com>
2023-11-06 00:26:48 -0500

    Candy counting program

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int blueCandies = 102;
+        int redCandies = 100;
+        int greenCandies = 94;
+
+        int totalCandies = blueCandies + redCandies + greenCandies;
+
+        System.out.println("Total number of candies: " + totalCandies);
+    }
+}
